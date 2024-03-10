c67f66663aaff88b525451caeffd110fe55ac124
roham996 <153001440+roham996@users.noreply.github.com>
2024-03-10 20:01:27 +0330

    Update Proxy-tester.py

--- Proxy-tester.py
+++ Proxy-tester.py
@@ -1,16 +1,19 @@
 import requests
+from colorama import Fore
 
 input_proxy = input('Enter your proxy file(No need add .txt): ')
 input_proxy = input_proxy + '.txt'
 
-with open(input_proxy,'r') as file:
-    proxys = file.readlines()
-    
-    
-for proxy in proxys:
+with open(input_proxy, 'r') as file:
+    proxies = file.readlines()
+
+for proxy in proxies:
     proxy = proxy.strip()
     proxy = {'http': 'http://' + proxy}
 
-    r = requests.get(url='https://chamranvoc.ir', proxies=proxy)
-    with open('ashhsd.html','w',encoding='utf8') as file:
-        file.write(r.text)
+
+    response = requests.get(url='https://google.com', proxies=proxy)
+    if response.status_code == 200:
+        print(Fore.GREEN + 'Proxy is work: ' + proxy['http'])
+    else:
+       print(Fore.RED + 'This proxy not work: ' + proxy['http'] + '    Error code: ' + str(response.status_code))
